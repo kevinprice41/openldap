8f95b8817090703ab1b68af365e0472297b52e03
prizyp <kevinprice41@gmail.com>
2015-09-21 14:17:24 -0400

    Update start.sh

--- script/start.sh
+++ script/start.sh
@@ -43,42 +43,79 @@ access to *
         by * none
 EOF
 
-cat >>/etc/ldap/schema/pwm.schema <<EOF
-# /etc/ldap/schema/pwm.schema
-#
+cat >>/etc/ldap/schema/openldap.ldif <<EOF
+dn: cn=pwm,cn=schema,cn=config
+objectClass: olcSchemaConfig
+cn: pwm
+olcAttributeTypes: {0}( 1.3.6.1.4.1.35015.1.2.1 NAME 'pwmEventLog' E
+ QUALITY octetStringMatch SYNTAX 1.3.6.1.4.1.1466.115.121.1.40 )
+olcAttributeTypes: {1}( 1.3.6.1.4.1.35015.1.2.2 NAME 'pwmResponseSet
+ ' EQUALITY octetStringMatch SYNTAX 1.3.6.1.4.1.1466.115.121.1.40 )
+olcAttributeTypes: {2}( 1.3.6.1.4.1.35015.1.2.3 NAME 'pwmLastPwdUpda
+ te' SYNTAX 1.3.6.1.4.1.1466.115.121.1.24 )
+olcAttributeTypes: {3}( 1.3.6.1.4.1.35015.1.2.4 NAME 'pwmGUID' SYNTA
+ X 1.3.6.1.4.1.1466.115.121.1.15 )
+olcAttributeTypes: {1}( 1.3.6.1.4.1.35015.1.2.6 NAME 'pwmOtpSecret '
+ EQUALITY octetStringMatch SYNTAX 1.3.6.1.4.1.1466.115.121.1.40 )
+olcObjectClasses: {0}( 1.3.6.1.4.1.591242.1.2010.04.16.1 NAME 'pwmUser' AUXILI
+ ARY MAY ( pwmLastPwdUpdate $ pwmEventLog $ pwmResponseSet $ pwmOtpSecret $ pw
+ mGUID ) )
+EOF
+
+cat >>/etc/ldap/schema/openldap.schema <<EOF
 # We try to define OID's "correctly" as outlined here:
 #
 # http://www.openldap.org/doc/admin23/schema.html
 #
-# 1.3.6.1.4.1   base OID
-# 591242        organization idenfifier
-# 1             if an objectclass
-# 2             if an attribute
-# yyyy.mm.dd    date of creation
-# n             extra identifier
+# 1.3.6.1.4.1 base OID
+# 35015 organization idenfifier
+# 1.1 if an objectclass
+# 1.2 if an attribute
+# n extra identifier
 #
-attributetype ( 1.3.6.1.4.1.591242.2.2010.04.16.1
+attributetype ( 1.3.6.1.4.1.35015.1.2.1
         NAME 'pwmEventLog'
+        EQUALITY octetStringMatch
         SYNTAX 1.3.6.1.4.1.1466.115.121.1.40 )
 
-attributetype ( 1.3.6.1.4.1.591242.2.2010.04.16.2
+attributetype ( 1.3.6.1.4.1.35015.1.2.2
         NAME 'pwmResponseSet'
+        EQUALITY octetStringMatch
         SYNTAX 1.3.6.1.4.1.1466.115.121.1.40 )
 
-attributetype ( 1.3.6.1.4.1.591242.2.2010.04.16.3
+attributetype ( 1.3.6.1.4.1.35015.1.2.3
         NAME 'pwmLastPwdUpdate'
         SYNTAX 1.3.6.1.4.1.1466.115.121.1.24 )
 
-attributetype ( 1.3.6.1.4.1.591242.2.2010.04.16.4
+attributetype ( 1.3.6.1.4.1.35015.1.2.4
         NAME 'pwmGUID'
         SYNTAX 1.3.6.1.4.1.1466.115.121.1.15 )
 
-objectclass ( 1.3.6.1.4.1.591242.1.2010.04.16.1
+attributetype ( 1.3.6.1.4.1.35015.1.2.6
+        NAME 'pwmOtpSecret'
+        EQUALITY octetStringMatch
+        SYNTAX 1.3.6.1.4.1.1466.115.121.1.40 )
+
+objectclass ( 1.3.6.1.4.1.35015.1.1.1
         NAME 'pwmUser'
         AUXILIARY
-        MAY ( pwmLastPwdUpdate $ pwmEventLog $ pwmResponseSet $ pwmGUID ))
+        MAY ( pwmLastPwdUpdate $ pwmEventLog $ pwmResponseSet $
+                pwmOtpSecret $ pwmGUID ) )
 EOF
 
+cat >>/etc/ldap/base.ldif <<EOF
+dn: dc=example,dc=com
+objectClass: dcObject
+objectClass: organization
+o: thegeekstuff.com
+dc: thegeekstuff
+dn: ou=Accounts,dc=example,dc=com
+objectClass: organizationalUnit
+objectClass: top
+ou: Accounts
+EOF
+
+  ldapadd -x -W -D "cn=admin,dc=example,dc=com" -w secret -f /etc/ldap/base.ldif
 
   DEBIAN_FRONTEND=noninteractive dpkg-reconfigure -f noninteractive slapd
 
